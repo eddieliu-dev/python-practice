3342d686678cfc5c245da5cd60daf0488afb3482
Eddie Liu <el3798@nyu.edu>
2025-07-28 12:57:40 +0800

    list_processing function created and works

--- fundamentals.py
+++ fundamentals.py
@@ -1,5 +1,6 @@
 import random
 
+
 # Hello and Age
 # Asks the user for their name and age
 # Prints: "Hello [name], you will be [age + 1] next year!"
@@ -66,42 +67,61 @@ def guess_the_number():
             guess = int(input("Guess again: "))
     print("Correct!")
 
+
 # List Processing
 # Given a list of numbers: [5, 3, 8, 6, 7, 2]
 # Find the maximum, minimum, and average
 # Sort the list in descending order
+def list_processing():
+    list = [5, 3, 8, 6, 7, 2]
+    max = list[0]
+    min = list[0]
+    sum = 0
+    for i in range(len(list)):
+        if list[i] > max:
+            max = list[i]
+        if list[i] < min:
+            min = list[i]
+        sum += list[i]
+    print(list)
+    print("max=" + str(max) + ", min=" + str(min) + ", average=" + str(sum / len(list)))
+    list.sort(reverse=True)
+    print(list)
 
-# Student Scores Dictionary
-# Create a dictionary storing student names and their scores
-# Add at least 3 students
-# Print the student with the highest score
+    # Student Scores Dictionary
+    # Create a dictionary storing student names and their scores
+    # Add at least 3 students
+    # Print the student with the highest score
 
-# Word Count in a Sentence
-# Ask for a sentence
-# Count how many times each word appears
 
-# Temperature Converter Function
-# Write two functions:
-# celsius_to_fahrenheit(c)
-# fahrenheit_to_celsius(f)
+    # Word Count in a Sentence
+    # Ask for a sentence
+    # Count how many times each word appears
 
-# Read and Count Words from File
-# Write a text file manually (data.txt)
-# Write a script to open the file, read the content, and count how many words it has
+    # Temperature Converter Function
+    # Write two functions:
+    # celsius_to_fahrenheit(c)
+    # fahrenheit_to_celsius(f)
 
-# Contact Book
-# Allow user to:
-# Add contact (name, phone)
-# Search for contact
-# List all contacts
-# Save the data to a text file (one contact per line)
+    # Read and Count Words from File
+    # Write a text file manually (data.txt)
+    # Write a script to open the file, read the content, and count how many words it has
+
+    # Contact Book
+    # Allow user to:
+    # Add contact (name, phone)
+    # Search for contact
+    # List all contacts
+    # Save the data to a text file (one contact per line)
+
 
 def main():
     # hello_and_age()
     # calculator()
     # odd_or_even()
     # multiplication_table()
-    guess_the_number()
+    # guess_the_number()
+    list_processing()
 
 
 if __name__ == "__main__":
