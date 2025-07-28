dff9fc5f09426b83e17aad02a3f1bd9dee69352a
Eddie Liu <el3798@nyu.edu>
2025-07-28 10:56:29 +0800

    oddoreven function created and works

--- fundamentals.py
+++ fundamentals.py
@@ -6,6 +6,7 @@ def hello_and_age():
     age = input("What is your age? ")
     print("Hello " + name + ", you will be " + str(int(age) + 1) + " next year!")
 
+
 # Calculator
 # Takes two numbers and an operator (+, -, *, /) as input
 # Prints the result
@@ -13,7 +14,7 @@ def calculator():
     calculator = input("Calculator initiated:\n")
     if "+" in calculator:
         first_num, second_num = calculator.split("+")
-        print(float(first_num)+float(second_num))
+        print(float(first_num) + float(second_num))
     elif "-" in calculator:
         first_num, second_num = calculator.split("-")
         print(float(first_num) - float(second_num))
@@ -24,9 +25,61 @@ def calculator():
         first_num, second_num = calculator.split("/")
         print(float(first_num) / float(second_num))
 
+
+# Odd or Even
+# Ask the user for a number
+# Print whether it’s odd or even
+def odd_or_even():
+    num = input("Give me a number: ")
+    if int(num) % 2 == 0:
+        print(num + " is even.")
+    else:
+        print(num + " is odd.")
+
+
+# Multiplication Table
+# Ask for a number n
+# Print its multiplication table up to 10
+
+# Guess the Number
+# Generate a random number between 1 and 20
+# Let the user guess until they get it right
+# Show how many attempts it took
+
+# List Processing
+# Given a list of numbers: [5, 3, 8, 6, 7, 2]
+# Find the maximum, minimum, and average
+# Sort the list in descending order
+
+# Student Scores Dictionary
+# Create a dictionary storing student names and their scores
+# Add at least 3 students
+# Print the student with the highest score
+
+# Word Count in a Sentence
+# Ask for a sentence
+# Count how many times each word appears
+
+# Temperature Converter Function
+# Write two functions:
+# celsius_to_fahrenheit(c)
+# fahrenheit_to_celsius(f)
+
+# Read and Count Words from File
+# Write a text file manually (data.txt)
+# Write a script to open the file, read the content, and count how many words it has
+
+# Contact Book
+# Allow user to:
+# Add contact (name, phone)
+# Search for contact
+# List all contacts
+# Save the data to a text file (one contact per line)
+
 def main():
     # hello_and_age()
-    calculator()
+    # calculator()
+    odd_or_even()
 
 if __name__ == "__main__":
     main()
