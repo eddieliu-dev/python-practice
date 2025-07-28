a467069f3090544b93e596713103b38d6d43df22
Eddie Liu <el3798@nyu.edu>
2025-07-28 09:56:56 +0800

    fundamentals practice file created with two prac functions

---
+++ fundamentals.py
@@ -0,0 +1,18 @@
+# Hello and Age
+# Asks the user for their name and age
+# Prints: "Hello [name], you will be [age + 1] next year!"
+def hello_and_age():
+    name = input("What is your name? ")
+    age = input("What is your age? ")
+    print("Hello " + name + ", you will be " + str(int(age) + 1) + " next year!")
+
+# Calculator
+# Takes two numbers and an operator (+, -, *, /) as input
+# Prints the result
+def calculator():
+    calculator = input("Calculator initiated:\n")
+
+
+def main():
+    # hello_and_age()
+    calculator()
